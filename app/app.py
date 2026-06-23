932b4001b8e6d3067a4f3d804e7b4246c6e2ca76
Reshufowzi <114790414+Reshufowzi@users.noreply.github.com>
2026-06-23 19:23:54 +0530

    Update app.py

--- app/app.py
+++ app/app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return jsonify({"message": "Hello from DevOps neww6798789889wwwwwww Project 🚀"})
+    return jsonify({"message": "Hello from DevOps neww67987890987779889wwwwwww Project 🚀"})
 
 @app.route('/health')
 def health():
